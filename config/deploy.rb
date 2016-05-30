226c85ddc68f7642943f9bcdd39fb4f05a681ddf
se7enxin <se7enxin@yahoo.com>
2016-05-30 23:23:27 +0800

    test capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'example.com/my_blog'
+# set :application, 'example.com/my_blog'
 set :repo_url, 'git@github.com:se7enxin/my_blog.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/example.com/my_blog'
 
 # Default value for :scm is :git
 # set :scm, :git
